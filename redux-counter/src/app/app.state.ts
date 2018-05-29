eb8580f13cb97bbf1e42934e423947b20df22565
DimaAdmin <ryba-mech@yandex.ru>
2018-05-29 15:38:14 +0300

    10 - create new app "redux-counter" and define state App

--- redux-counter/src/app/app.state.ts
+++ redux-counter/src/app/app.state.ts
@@ -3,7 +3,6 @@
  *
  * In this case, our app state is simply a single number (the counter). But we
  * put it here because in the future, when our state is more complicated
- *
  */
 
 export interface AppState {
